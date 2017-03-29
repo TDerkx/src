4a123927aad57dc7374fabccf40ff7b941612732
TDerkx <tomderkxtue@gmail.com>
2017-03-29 12:30:13 +0200

    Map works!
    now only need to implement additional functionality neccesary for our
    map.

--- main/java/nl/tue/vagariapp/Tabs.java
+++ main/java/nl/tue/vagariapp/Tabs.java
@@ -24,7 +24,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by Stijn Albert  on 19-3-2017.
  */
 
-public class Tabs extends FragmentActivity {
+public class Tabs extends FragmentActivity  implements OnMapReadyCallback {
 
     public TabHost th;
 
@@ -33,7 +33,6 @@ public class Tabs extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabs);
-
 
 
         th = (TabHost) findViewById(R.id.tabHost);
@@ -54,6 +53,19 @@ public class Tabs extends FragmentActivity {
         th.addTab(specs);
 
         th.setCurrentTab(1);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mapFragment.onResume(); // needed to get the map to display immediately;
+            /* the next part should implement moving.
+
+            mapFragment.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            mapFragment.getUiSettings().setCompassEnabled(true);
+            mapFragment.getUiSettings().setZoomControlsEnabled(true);
+            mapFragment.getUiSettings().setZoomGesturesEnabled(true);
+            mapFragment.getUiSettings().setScrollGesturesEnabled(true);
+            */
 
         for (int i = 0; i < th.getTabWidget().getChildCount(); i++) {
             TextView tv = (TextView) th.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
@@ -83,49 +95,17 @@ public class Tabs extends FragmentActivity {
                 startActivity(intent);
             }
         });
-        th.getTabWidget().getChildAt(2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Tabs.this, Map.class);
-                startActivity(intent);
-            }
-        });
-
 
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
-    public class Map extends FragmentActivity implements OnMapReadyCallback {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            setContentView(R.layout.tabs);
-            MapFragment mapFragment = (MapFragment) getFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-            mapFragment.onResume(); // needed to get the map to display immediately;
-            /* the next part should implement moving.
-
-            mapFragment.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            mapFragment.getUiSettings().setCompassEnabled(true);
-            mapFragment.getUiSettings().setZoomControlsEnabled(true);
-            mapFragment.getUiSettings().setZoomGesturesEnabled(true);
-            mapFragment.getUiSettings().setScrollGesturesEnabled(true);
-            */
-
-        }
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-
-        }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-
 }
-
-
