e37fa514ba3eae45becb78271d95536af825ee67
St1ko <stijnalbert@hotmail.com>
2017-04-02 23:07:18 +0200

    Custom Imageview with square ratio

---
+++ main/java/nl/tue/vagariapp/SquareImageView.java
@@ -0,0 +1,28 @@
+package nl.tue.vagariapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Stijn Albert on 2-4-2017.
+ */
+
+//Custom ImageView with square ratio
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
+    }
+}
